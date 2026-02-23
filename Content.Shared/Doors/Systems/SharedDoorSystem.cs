600615fb1f64df3b9f71d0e6695a9cd35cdc0885
Sage Jones <sagesterjones@gmail.com>
2026-02-22 23:47:18 -0600

    Revert "Make door bolting powergaming no longer relevant anymore (#41138)"
    This reverts commit 67e18a0693c501f43b15fc8705720d7b6e14f31d.

--- Content.Shared/Doors/Systems/SharedDoorSystem.cs
+++ Content.Shared/Doors/Systems/SharedDoorSystem.cs
@@ -131,7 +131,7 @@ public abstract partial class SharedDoorSystem : EntitySystem
         if (!TryComp<AirlockComponent>(uid, out var airlock))
             return;
 
-        if (!airlock.Powered)
+        if (IsBolted(uid) || !airlock.Powered)
             return;
 
         if (door.State != DoorState.Closed)
@@ -406,7 +406,7 @@ public abstract partial class SharedDoorSystem : EntitySystem
             Audio.PlayPvs(door.OpenSound, uid, AudioParams.Default.WithVolume(-5));
 
         if (lastState == DoorState.Emagging && TryComp<DoorBoltComponent>(uid, out var doorBoltComponent))
-            SetBoltsDown((uid, doorBoltComponent), true, user, true);
+            SetBoltsDown((uid, doorBoltComponent), !doorBoltComponent.BoltsDown, user, true);
     }
 
     /// <summary>
